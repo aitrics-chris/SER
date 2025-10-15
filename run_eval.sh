253c16b713b55857bd8dff5446ac2aed25724c43
이주형 <43436960+aitrics-chris@users.noreply.github.com>
2025-10-15 20:04:19 +0900

    Update run_eval.sh

--- run_eval.sh
+++ run_eval.sh
@@ -1,2 +1,1 @@
-unset LD_LIBRARY_PATH
-CUDA_VISIBLE_DEVICES=0,1,2,4,5,6,7 PYTHONHASHSEED=1 python lincls_parallel.py --epochs 30 --val_epoch 27 --config eval1.yaml --seed 1
+CUDA_VISIBLE_DEVICES=0,1,2,4,5,6,7 PYTHONHASHSEED=1 python lincls_parallel.py --epochs 50 --val_epoch 40 --config eval1.yaml --seed 1
